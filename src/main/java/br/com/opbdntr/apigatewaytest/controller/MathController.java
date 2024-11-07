b6e1bd8a6716ec7f585e77b11c096b69e5ec6929
Rikelvem <rikelvemrsilva@gmail.com>
2024-11-07 03:40:05 -0300

    refatorando o codigo

--- src/main/java/br/com/opbdntr/apigatewaytest/controller/MathController.java
+++ src/main/java/br/com/opbdntr/apigatewaytest/controller/MathController.java
@@ -1,6 +1,8 @@
-package br.com.opbdntr.apigatewaytest;
+package br.com.opbdntr.apigatewaytest.controller;
 
+import br.com.opbdntr.apigatewaytest.converter.NumberConverter;
 import br.com.opbdntr.apigatewaytest.exceptions.MathOperationException;
+import br.com.opbdntr.apigatewaytest.math.SimpleMath;
 import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -8,50 +10,51 @@ import java.util.concurrent.atomic.AtomicLong;
 public class MathController {
 
     private final AtomicLong counter = new AtomicLong();
+    private SimpleMath math = new SimpleMath();
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne,
                       @PathVariable(value = "numberTwo") String numberTwo)
-                      throws Exception
+            throws Exception
     {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new MathOperationException("Necessário ser numero!");
         }
 
-        return converterNumber(numberOne) + converterNumber(numberTwo);
+        return math.sum(NumberConverter.converterNumber(numberOne), NumberConverter.converterNumber(numberTwo));
     }
 
     @RequestMapping(value = "sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sub(@PathVariable(value = "numberOne") String numberOne,
                       @PathVariable(value = "numberTwo") String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new MathOperationException("Use apenas numeros");
         }
 
-        return converterNumber(numberOne) - converterNumber(numberTwo);
+        return math.sub(NumberConverter.converterNumber(numberOne), NumberConverter.converterNumber(numberTwo));
     }
 
     @RequestMapping(value = "mut/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     private Double mut(@PathVariable(value = "numberOne") String numberOne,
                        @PathVariable(value = "numberTwo") String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new MathOperationException("Use apenas numeros");
         }
 
-        return converterNumber(numberOne) * converterNumber(numberTwo);
+        return math.mut(NumberConverter.converterNumber(numberOne), NumberConverter.converterNumber(numberTwo));
     }
 
     @RequestMapping(value = "div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     private Double div (@PathVariable(value = "numberOne")String numberOne,
                         @PathVariable(value = "numberTwo")String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new MathOperationException("Use apenas numeros");
         }
 
-        return converterNumber(numberOne) / converterNumber(numberTwo);
+        return math.div(NumberConverter.converterNumber(numberOne), NumberConverter.converterNumber(numberTwo));
     }
 
     @RequestMapping(value = "/med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
@@ -59,42 +62,20 @@ public class MathController {
                       @PathVariable(value = "numberTwo") String numberTwo)
             throws Exception
     {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new MathOperationException("Necessário ser numero!");
         }
 
-        return (converterNumber(numberOne) + converterNumber(numberTwo)) / 2;
+        return math.med(NumberConverter.converterNumber(numberOne), NumberConverter.converterNumber(numberTwo));
     }
 
     @RequestMapping(value = "raiz/{numberOne}", method = RequestMethod.GET)
     private Double raiz (@PathVariable(value = "numberOne") String numberOne) throws Exception {
-        if (!isNumeric(numberOne)){
+        if (!NumberConverter.isNumeric(numberOne)){
             throw new MathOperationException("Use apenas numeros");
         }
 
-        return tiraRaiz(numberOne);
-    }
-
-    private Double converterNumber(String strNumber) throws Exception {
-        replaceNumber(strNumber);
-        if (isNumeric(strNumber)) return Double.parseDouble(strNumber);
-        return 0D;
-    }
-
-    private boolean isNumeric(String strNumber) throws Exception {
-        replaceNumber(strNumber);
-        return strNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
-
-    private String replaceNumber(String strNumber) throws Exception{
-        if (strNumber == null) throw new MathOperationException("Não aceitamos numeros nulos");
-        return strNumber.replaceAll(",", ".");
-    }
-
-    private Double tiraRaiz(String number) throws Exception {
-        replaceNumber(number);
-        double result = Double.parseDouble(number);
-        return Math.sqrt(result);
+        return math.raiz(NumberConverter.converterNumber(numberOne));
     }
 
 }
