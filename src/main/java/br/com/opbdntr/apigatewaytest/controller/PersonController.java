1d395e2a93f5fe2c8e4c5acb85c164a2c1a85a82
Rikelvem <rikelvemrsilva@gmail.com>
2025-01-20 11:47:32 -0300

    incrementando verbos e chamadas

--- src/main/java/br/com/opbdntr/apigatewaytest/controller/PersonController.java
+++ src/main/java/br/com/opbdntr/apigatewaytest/controller/PersonController.java
@@ -4,10 +4,7 @@ import br.com.opbdntr.apigatewaytest.model.Person;
 import br.com.opbdntr.apigatewaytest.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +21,24 @@ public class PersonController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") String id) {
         return service.findById(id);
     }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person createPerson(@RequestBody Person person) {
+        return service.createPerson(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person updatePerson(@RequestBody Person person) {
+        return service.updatePerson(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletePerson(@PathVariable(value = "id") String id) {
+        service.deletePerson(id);
+    }
+
 
 }
