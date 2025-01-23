8b9ead757044a5c2a216e13bf846626c982d65dd
Rikelvem <rikelvemrsilva@gmail.com>
2025-01-23 00:23:20 -0300

    corrigindo bugs da service

--- src/main/java/br/com/opbdntr/apigatewaytest/service/PersonService.java
+++ src/main/java/br/com/opbdntr/apigatewaytest/service/PersonService.java
@@ -1,6 +1,7 @@
 package br.com.opbdntr.apigatewaytest.service;
 
 import br.com.opbdntr.apigatewaytest.data.vo.v1.PersonVO;
+import br.com.opbdntr.apigatewaytest.mapper.DozerMapper;
 import br.com.opbdntr.apigatewaytest.model.Person;
 import br.com.opbdntr.apigatewaytest.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.logging.Logger;
+
+import static br.com.opbdntr.apigatewaytest.mapper.DozerMapper.parseListObjects;
+import static br.com.opbdntr.apigatewaytest.mapper.DozerMapper.parseObject;
 
 @Service
 public class PersonService {
@@ -20,29 +24,28 @@ public class PersonService {
     public List<PersonVO> findAll() {
         logger.info("Finding all people!");
 
-        return repository.findAll();
+        return parseListObjects(repository.findAll(), PersonVO.class);
     }
 
     public PersonVO findById (Long id) {
-
         logger.info("Finding by id!");
 
-        PersonVO person = new PersonVO();
-        person.setFirstName("Rikelvem");
-        person.setLastName("Silva");
-        person.setAddress("São Paulo - SP - Brasil");
-        person.setGender("Male");
-        return repository.findById(id)
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResolutionException("No records found for this ID!"));
+        return parseObject(entity, PersonVO.class);
     }
 
     public PersonVO createPerson (PersonVO person) {
         logger.info("Creating one person!");
-        return repository.save(person);
+
+        var entity = parseObject(person, Person.class);
+        return parseObject(repository.save(entity), PersonVO.class);
     }
 
     public PersonVO updatePerson (PersonVO person) {
-        PersonVO entity = repository.findById(person.getId())
+        logger.info("Updating one person!");
+
+        var entity = repository.findById(person.getId())
                 .orElseThrow(() -> new ResolutionException("No records found for this ID!"));
 
         entity.setFirstName(person.getFirstName());
@@ -50,11 +53,13 @@ public class PersonService {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(person);
+        return parseObject(repository.save(entity), PersonVO.class);
     }
 
     public void deletePerson (Long id) {
-        PersonVO entity = repository.findById(id)
+        logger.info("Deleting one person!");
+
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResolutionException("No records found for this add."));
         repository.delete(entity);
     }
