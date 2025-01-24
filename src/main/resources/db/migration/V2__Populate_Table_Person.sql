999f807f66ec30b22d148c8ec1a36375856cce88
Rikelvem <rikelvemrsilva@gmail.com>
2025-01-24 17:38:30 -0300

    incrementacao do flyway no codigo para versionamento do db

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Cidade Tiradentes', 'Marcos', 'Male', 'Vinicius'),
+	(2, 'Rio de Janeiro', 'Rikelvem', 'Male', 'Silva'),
+	(4, 'Passaro de fogo', 'Paula', 'Female', 'Fernandes'),
+	(7, 'Minas Gerais - Brasil', 'Patricia', 'Female', 'Abravanel'),
+	(8, 'Cidade Tiradentes', 'Marcos', 'Male', 'Vinicius'),
+	(9, 'Cidade Tiradentes', 'Marcos', 'Male', 'Pedro'),
+	(10, 'Cidade Azul', 'Joao', 'Male', 'Pedro');
